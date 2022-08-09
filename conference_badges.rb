f25d8fd242bcf84c6ddaabb7b182e7a33c0b652b
Eriche Gonzales <erichelouise@gmail.com>
2022-08-09 17:14:27 -0400

    commit tree de933801e2f32c5abb844872ee9552bc31ab09ce

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker name
+    "Hello, my name is #{name.capitalize}."
+end
+
+def batch_badge_creator names
+    names.map do |name|
+        "Hello, my name is #{name.capitalize}."
+    end
+end
+
+def assign_rooms names
+    names.map.with_index(1) do |name, room| 
+        "Hello, #{name.capitalize}! You'll be assigned to room #{room}!"  
+    end
+end
+
+def printer names
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
